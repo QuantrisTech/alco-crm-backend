a52b4b179dd9fc0d4f6961a5f0c67529488530c2
Khansa <aiengineer.quantristech@gmail.com>
2026-07-13 15:40:44 +0500

    Add updateVisitor endpoint for editing visitor details before lead conversion

--- routes/visitorRoute.js
+++ routes/visitorRoute.js
@@ -3,13 +3,15 @@ const { authorize } = require("../middlewares/roleMiddleware");
 const express = require("express");
 const router = express.Router();
 const verifyChatbotKey = require("../middlewares/verifyChatbotKey");
-const { upsertVisitor, promoteVisitor, getAllVisitors } = require("../controllers/visitorController");
+const { upsertVisitor, promoteVisitor, getAllVisitors, updateVisitor } = require("../controllers/visitorController");
 
 router.post("/", verifyChatbotKey, upsertVisitor);
 
 router.post("/:id/promote", protect, authorize("super_admin", "admin"), promoteVisitor);
 
 router.get("/", protect, authorize("super_admin", "admin"), getAllVisitors);
+
+router.patch("/:id", protect, authorize("super_admin", "admin"), updateVisitor);
 
 module.exports = router;
 
